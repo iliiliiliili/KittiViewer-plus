6fdfc7a9c93212ba3d5184432a8887ff79b1963c
Illia Oleksiienko <iliiliilya123@gmail.com>
2020-06-10 17:28:10 +0200

    Init

---
+++ README.md
@@ -0,0 +1,112 @@
+# Python KittiViewer+
+
+This is an upgrade of [Second.KittiViewer](https://github.com/traveller59/second.pytorch#try-kitti-viewer-deprecated)
+
+- Works on raw KITTI data (without `second` preprocessing)
+- Allows to compare results of different networks (results must be saved in KITTI format)
+- Allows to display segmentation of points
+
+## Install Dependencies
+
+``pip install numba dill matplotlib fire shapely scikit-image PyOpenGL protobuf``
+
+It's easier to use [conda](https://docs.conda.io/en/latest/miniconda.html) to get QT:
+
+``conda install qt pyqt pyqtgraph``
+
+### QT errors
+- If you have `“Failed to load platform plugin ”xcb“ ” while launching qt5 app on linux without qt installed` error, it is probably because libxcb-xinerama0 file is not found. You can try run `sudo apt install --reinstall libxcb-xinerama0` to fix it.
+
+## Run
+
+`python viewer.py`
+
+## Parameters 
+
+All parameters are stored in `params.json`.
+
+## KITTI Detection
+
+### Data setup
+
+- `kitti_detection_root` parameter contains a path for detection KITTI dataset. You must have `image_2`, `label_2`, `velodyne`, `calib` folders inside `"selected_split": "training"` folder.
+
+    `"kitti_detection_root": "/data/sets/kitti_second"`
+
+- `folders` parameter contains names for each modality (if you named them differently from KITTI).
+
+    ```json
+    "folders": {
+        "detection": {
+            "calib": "calib",
+            "image": "image_2",
+            "label": "label_2",
+            "velodyne": "velodyne"
+        },
+    }
+    ```
+
+- `compare_results_detection` object describes how to display results from different methods. All results must be stored inside the `root` folder and `methods[i]["name"]` subfolder in KITTI annotations format (.txt files as in `label_2`). `color` field describes RGB color of 3d and 2d boxes.
+
+    ```json
+        "compare_results_detection": {
+
+            "root": "/data/sets/kitti_results_detection",
+            "methods": [
+
+                {
+                    "name": "method1",
+                    "color": [1.0, 0.0, 1.0]
+                },
+                {
+                    "name": "method2",
+                    "color": [1.0, 1.0, 0.0]
+                },
+                {
+                    "name": "method3",
+                    "color": [0.0, 0.0, 1.0]
+                }
+            ]
+        }
+    ```
+
+### Example
+
+![Detection example](./images/detection.png)
+
+
+## KITTI Tracking
+
+### Data setup
+
+- `kitti_tracking_root` parameter contains a path for detection KITTI dataset. You must have `image_2`, `label_2`, `velodyne`, `calib` folders inside `"selected_split": "training"` folder. `image_2` and `velodyne` must have folders for each scene.
+
+    `"kitti_tracking_root": "/data/sets/kitti_tracking"`
+
+- `folders` parameter contains names for each modality (if you named them differently from KITTI).
+
+    ```json
+    "folders": {
+        "tracking": {
+            "calib": "calib",
+            "image": "image_2",
+            "label": "label_2",
+            "velodyne": "velodyne"
+        },
+    }
+    ```
+### Example
+
+![Detection example](./images/tracking.png)
+
+## KITTI Segmentation (In development)
+
+### Data setup
+
+- `kitti_segmentation_root` parameter contains a path for detection KITTI dataset. You must have `.bin` files `"selected_split": "training"` folder. Each file is created using `dill` library and must contain `segmented_points` (`NxC`, C - amount of classes (currently C=1 only),`original_points` (`Nx3` - with points coordinates), `ground_trurh_points` (`NxC` - with ground truth segmentation)
+
+    `"kitti_segmentation_root": "/data/sets/kitti_segmentation"`
+
+### Example
+
+![Detection example](./images/segmentation.png)
---
+++ core.py
@@ -0,0 +1,236 @@
+import os
+import json
+from typing import Any, Callable, Dict, List, TypeVar, Union
+from kitti_common import (
+    read_lines,
+    parse_calib,
+    get_label_anno,
+)
+from inspect import signature
+import dill
+import numpy as np
+
+
+with open('params.json') as f:
+    kitti_params = json.load(f)
+
+detection_root = kitti_params['kitti_detection_root']
+tracking_root = kitti_params['kitti_tracking_root']
+segmentation_root = kitti_params['kitti_segmentation_root']
+
+if 'compare_results_detection' in kitti_params:
+    compare_results_detection = kitti_params['compare_results_detection']
+    compare_results_detection_methods = compare_results_detection['methods']
+else:
+    compare_results_detection = None
+
+detection_folders = kitti_params['folders']['detection']
+tracking_folders = kitti_params['folders']['tracking']
+selected_split = kitti_params['selected_split']
+
+T = TypeVar('T')
+T1 = TypeVar('T1')
+T2 = TypeVar('T2')
+T3 = TypeVar('T3')
+
+
+def arrays_dict_element(dict: Dict[T, List[Any]], index: int):
+    result = {}
+
+    for key, val in dict.items():
+        result[key] = val[index]
+    
+    return result
+
+
+def map_dict(
+    dict: Dict[T1, T2],
+    func: Union[Callable[[T2], T3], Callable[[T2, T1], T3]]
+):
+    result = {}
+
+    func_params_count = len(signature(func).parameters)
+
+    if func_params_count == 1:
+        for key, val in dict.items():
+            result[key] = func(val)  # type: ignore
+    elif func_params_count == 2:
+        for key, val in dict.items():
+            result[key] = func(val, key)  # type: ignore
+    else:
+        raise ValueError('Wrong reduce function')
+
+    return result
+
+
+
+def get_kitti_detection_files():
+
+    result = {}
+
+    for categoty, folder in detection_folders.items():
+        path = detection_root + '/' + selected_split + '/' + folder
+        result[categoty] = os.listdir(path) if os.path.isdir(path) else []
+        result[categoty].sort()
+        result[categoty] = list(map(
+            lambda a: path + '/' + a,
+            result[categoty]
+        ))
+
+    return result
+
+
+def get_kitti_tracking_files():
+
+    result = {}
+
+    for categoty, folder in tracking_folders.items():
+        path = tracking_root + '/' + selected_split + '/' + folder
+
+        if categoty in ['calib', 'label']:
+            result[categoty] = os.listdir(path) if os.path.isdir(path) else []
+            result[categoty].sort()
+            result[categoty] = list(map(
+                lambda a: path + '/' + a,
+                result[categoty]
+            ))
+        else:
+            groups = os.listdir(path) if os.path.isdir(path) else []
+            group_data = []
+
+            for group in groups:
+                local_path = path + '/' + group
+                group_data.append(os.listdir(local_path) if os.path.isdir(local_path) else [])
+                group_data[-1].sort()
+                group_data[-1] = list(map(
+                    lambda a: local_path + '/' + a,
+                    group_data[-1]
+                ))
+        
+            result[categoty] = group_data
+
+    return result
+
+
+def get_kitti_segmentation_files():
+
+    result = []
+
+    path = segmentation_root
+    result = os.listdir(path) if os.path.isdir(path) else []
+    result.sort()
+    result = list(map(
+        lambda a: path + '/' + a,
+        result
+    ))
+
+    return result
+
+
+def create_detection_kitti_info(files: dict, index):
+    
+    calib = parse_calib(
+        read_lines(files['calib'])
+    )
+
+    label = get_label_anno(
+        read_lines(files['label'])
+    )
+
+
+    result = {
+        'image_idx': index,
+        'velodyne_path': files['velodyne'],
+        'img_path': files['image'],
+        # 'img_shape': None,
+        'calib/P0': calib['P'][0],
+        'calib/P1': calib['P'][1],
+        'calib/P2': calib['P'][2],
+        'calib/P3': calib['P'][3],
+        'calib/R0_rect': calib['R0_rect'],
+        'calib/Tr_velo_to_cam': calib['Tr_velo_to_cam'],
+        'calib/Tr_imu_to_velo': calib['Tr_velo_to_cam'],
+        'annos': label
+    }
+
+    return result
+        
+
+def create_tracking_kitti_info(files: dict, index):
+    
+    calib = parse_calib(
+        read_lines(files['calib'])
+    )
+
+    label_lines = read_lines(files['label'])
+
+    selected_frame_lines = []
+
+    for line in label_lines:
+
+        parts = line.split(' ')
+        frame = int(parts[0])
+
+        if frame == index and parts[1] != '-1':
+            selected_frame_lines.append(' '.join(parts[2:]))
+
+
+    label = get_label_anno(
+        selected_frame_lines
+    )
+
+
+    result = {
+        'image_idx': index,
+        'velodyne_path': files['velodyne'][index],
+        'img_path': files['image'][index],
+        # 'img_shape': None,
+        'calib/P0': calib['P'][0],
+        'calib/P1': calib['P'][1],
+        'calib/P2': calib['P'][2],
+        'calib/P3': calib['P'][3],
+        'calib/R0_rect': calib['R0_rect'],
+        'calib/Tr_velo_to_cam': calib['Tr_velo_to_cam'],
+        'calib/Tr_imu_to_velo': calib['Tr_velo_to_cam'],
+        'annos': label
+    }
+
+    return result
+
+
+def create_segmentation_kitti_info(file_name: str):
+    
+    with open(file_name, 'rb') as f:
+        segmented_points = dill.load(f)
+        original_points = dill.load(f)
+        ground_trurh_points = dill.load(f)
+
+    segmented_points = segmented_points[:len(original_points)]
+    ground_trurh_points = ground_trurh_points[:len(original_points)]
+
+    points = np.concatenate([original_points, segmented_points], axis=-1)
+    points = np.concatenate([points, ground_trurh_points], axis=-1)
+
+    result = {
+        'points': points
+    }
+
+    return result
+
+
+def get_compare_detection_annotation(method, index):
+    file_name = (
+        compare_results_detection['root'] + '/' + method + '/'
+        + selected_split + '/' + '{:06d}'.format(index) + '.txt'
+    )
+
+    lines = []
+
+    if os.path.exists(file_name):
+        lines = read_lines(file_name)
+    
+    label = get_label_anno(
+        lines
+    )
+
+    return label
